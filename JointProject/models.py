630e8dd43ccdb6bcd5f30cb664fd1e2f7545b115
albertm15 <155188920+albertm15@users.noreply.github.com>
2024-03-26 01:45:03 +0100

    UserRoomStaff model

--- JointProject/models.py
+++ JointProject/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 # Create your models here.
+STAFF_DOMAIN = "@laspalmeras.com"
+
+
+def validate_staff_email(email):
+    validate_email(email)
+    if not email.endswith(STAFF_DOMAIN):
+        raise ValidationError('El dominio del trabajador debe ser "' + STAFF_DOMAIN + '"')
+
+
+class UserRoomStaff(models.Model):
+    email = models.EmailField(validators=[validate_staff_email])
+    password = models.CharField(max_length=20, blank=False)
+    dni = models.CharField(max_length=9)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    second_surname = models.CharField(max_length=20, blank=True)
+    birthDate = models.DateField(null=True)
+
+    def __str__(self):
+        return self.email
