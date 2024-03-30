f0ccb72cc69945831431a75ffa4d4a8baff3c4fe
albertm15 <155188920+albertm15@users.noreply.github.com>
2024-03-30 01:40:37 +0100

    create + delete room bookings views

--- JointProject/views.py
+++ JointProject/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import RoomBookings
+from .forms import RoomBookingForm
 
 
 # Create your views here.
@@ -14,3 +15,23 @@ def obtainRoomBookings(request):
         'roomBookings' : roomBookings
     }
     return render(request, 'obtainRoomBookings.html', context)
+
+def createRoomBookings(request):
+    form = RoomBookingForm(request.POST)
+    context = {
+        'form' : form
+    }
+    if form.is_valid():
+        roomBooking = form.save(commit=False)
+        roomBooking.userWhoBooked = request.user
+        roomBooking.save()
+        return redirect('obtainRoomBookings')
+    return render(request, 'createRoomBooking.html', context)
+
+def deleteRoomBookings(request, roomBookingId):
+    roomBooking = get_object_or_404(RoomBookings,id=roomBookingId)
+    if request.method == 'POST':
+        roomBooking.delete()
+        return redirect('obtainRoomBookings')
+    else:
+        return HttpResponse("Bad Request: Only POST requests are allowed for this view.")
