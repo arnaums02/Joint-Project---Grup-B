744cd0a871bb47212c043219705b5d9020d3904b
arnaumarti <arnaums02@gmail.com>
2024-06-02 18:47:28 +0200

    to test

--- JointProject/tests.py
+++ JointProject/tests.py
@@ -173,7 +173,7 @@ class CleanRoomsTest(StaticLiveServerTestCase):
 
         self.assertEqual(roomBooking.toClean, True)
 
-        self.assertEqual(roomBooking.cleaned, True)
+        self.assertEqual(roomBooking.cleaned, False)
 
 
 
