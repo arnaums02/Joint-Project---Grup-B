d141db2207d5ee5a829f7a7c5b9b3f3bc43835f0
quimsb03 <quimsanchezbenet@gmail.com>
2024-06-14 00:44:59 +0200

    client views implemented

--- accounts/forms.py
+++ accounts/forms.py
@@ -50,7 +50,7 @@ class UserRegistrationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])
-        user.user_type = 'client'  # Establecer el tipo de usuario como 'client'
+        user.user_type = 'client'
         if commit:
             user.save()
         return user
